da29a89003102ec3d49640e6ef21a6f4b3a97388
Detsibli <jaimecabriales1@gmail.com>
2022-06-02 20:04:38 -0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,87 @@
+<script setup>
+import { ref, onMounted, computed, watch }from 'vue'
+
+const todos = ref([])
+const name = ref('')
+
+const input_contect = ref('')
+const input_category = ref(null)
+
+const todos_asc = computed(() => todos.value.sort((a, b) => {
+  return b.createdAt - a.createdAt  
+}))
+
+const addTodo = () => {
+  if (input_content.value.trim() === '' || input_category.value === null) {
+    return
+  }
+}
+
+watch(name, (newVal) => {
+  localStorage.setItem('name', newVal)
+})
+
+onMounted(() => {
+  name.value = localStorage.getItem('name') || ''
+})
+</script>
+
+<template>
+
+  <main class="app">
+    <section class="greeting">
+      <h2 class="title">
+        What's up, 
+        <input 
+          type="text" 
+          placeholder="Name here" 
+          v-model="name"/>
+      </h2>
+    </section>
+
+    <section class="create-todo">
+      <h3>Create a todo</h3>
+
+      <form @submit.prevent="addTodo">
+        <h4>What's on your todo list?</h4>
+
+        <input 
+          type="text" 
+          placeholder="e.g. make a video" 
+          v-model="input_content" />
+
+          <h4>Pick a category</h4>
+
+          <div class="options">
+
+            <label>
+              <input 
+                type="radio" 
+                name="category"
+                value="business"
+                v-model="input_category"
+              />
+              <span class="bubble business"></span>
+              <div>Business</div>
+            </label>
+
+            <label>
+              <input 
+                type="radio" 
+                name="category"
+                value="business"
+                v-model="input_category"
+              />
+              <span class="bubble personal"></span>
+              <div>Personal</div>
+            </label>
+
+          </div>
+
+        <input type="submit" value="Add todo">
+
+      </form>
+    </section>
+  </main>
+
+</template>
