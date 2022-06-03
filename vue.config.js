3928f648a67e753106c0ae5fe58842f5df33f711
Detsibli <jaimecabriales1@gmail.com>
2022-06-02 20:48:17 -0500

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? " Todo-App" : "./src/App.vue",
+  publicPath: process.env.NODE_ENV === "production" ? " Todo-App" : "/",
 };
